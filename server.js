6be998f113753f5e2e933e1d3d280df44ca1bb74
Tim Lebel <tim.lebel@gmail.com>
2015-02-05 21:24:45 -0800

    added make test

--- server.js
+++ server.js
@@ -4,22 +4,6 @@ var app = require('express')(),
 /* Configuration */
 app.use(serveStatic('www'));
 
-/* Connect to db */
-// mongoose.connect('mongodb://localhost/db-name', function (err) {
-//   if (err) {
-//     throw err;
-//   } else {
-//     console.log('Connected to MongoDB');
-//   }
-// });
-
-var zillow = require('./app/zillow');
-var neigh = 'Ballard';
-
-zillow.getNeighborhoodGroups(neigh, function (err, groups) {
-  console.dir(groups);
-});
-
 /* Listen */
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
