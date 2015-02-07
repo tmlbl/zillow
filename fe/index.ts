81c114682ac4067cf2467a95db645d0af3b5fee8
Tim Lebel <tim.lebel@gmail.com>
2015-02-07 14:35:43 -0800

    rudimentary hooking up buttons

--- fe/index.ts
+++ fe/index.ts
@@ -6,7 +6,6 @@ import TR = require('typed-react');
 import MapLookUp = require('maps/mapLookUp');
 import Map = require('maps/map');
 import Places = require('maps/places');
-import Loader = require('maps/mapDataset');
 import List = require('list/list')
 import TL = require('list/toggle-list');
 
@@ -14,7 +13,6 @@ function initialize() {
   Map.example(document.getElementById('content'), ()=>{
     React.render(React.createElement(MapLookUp.MapApp, {className: 'direction-forms'}), document.getElementById('form'));
   });
-  Loader.mapProjects();
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- fe/maps/mapDataset.ts
+++ fe/maps/mapDataset.ts
@@ -9,13 +9,34 @@ interface DataLoader {
     getHousingCounseling: (cb: (data: any[]) => any) => void;
 }
 
-export function mapProjects() {
+export function mapMultiFam() {
     Data.getMultiFamily((data) => {
         data = _.map(data, (datum) => {
             return {
                 position: new google.maps.LatLng(
                     datum.geometry['coordinates'][1],
                     datum.geometry['coordinates'][0]
+                ),
+                id: datum.name,
+                draggable: false,
+                raiseOnDrag: false,
+                icon: ' ',
+                labelContent: '<i class="fa fa-home" style="color:purple;"></i>',
+                labelAnchor: new google.maps.Point(0, 0),
+                labelClass: 'marker' // the CSS class for the label
+            };
+        });
+        $(document).trigger('markerData', [data]);
+    });
+}
+
+export function mapPublicHousing() {
+    Data.getProjects((data) => {
+        data = _.map(data, (datum) => {
+            return {
+                position: new google.maps.LatLng(
+                datum.geometry['coordinates'][1],
+                datum.geometry['coordinates'][0]
                 ),
                 id: datum.name,
                 draggable: false,
