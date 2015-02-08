29d3473c6bbae082553ce667e300a12c3ae87a39
Tim Lebel <tim.lebel@gmail.com>
2015-02-08 10:56:45 -0800

    doing big thangs

--- fe/index.ts
+++ fe/index.ts
@@ -18,20 +18,49 @@ function initialize() {
       setTimeout(()=> {
         // Define the LatLng coordinates for the polygon's path.
         var triangleCoords = [
-          new google.maps.LatLng(25.774252, -80.190262),
-          new google.maps.LatLng(18.466465, -66.118292),
-          new google.maps.LatLng(32.321384, -64.75737),
-          new google.maps.LatLng(25.774252, -80.190262)
+          new google.maps.LatLng(47.558105, -122.284539), // Rookies sports bar
+          new google.maps.LatLng(47.561175, -122.284089), // Rainier community center
+          new google.maps.LatLng(47.579174, -122.299633), // QFC Rainier Ave
+          new google.maps.LatLng(47.562967, -122.308345), // VA Puget Sound
+          new google.maps.LatLng(47.558105, -122.284539) // Rookies sports bar
         ];
+
+        var coordNames = [
+            'Rookie\'s Sports Bar',
+            'Rainier Community Center',
+            'QFC Rainier Avenue',
+            'VA Puget Sound'
+        ];
+
+        // Put down markers for the demo locations
+        function makeDemoMarker(latLng:google.maps.LatLng, name): Map.MarkerData {
+          return {
+            position: latLng,
+            id: name,
+            toolTip: name,
+            draggable: false,
+            raiseOnDrag: false,
+            icon: ' ',
+            labelContent: '<object style="width:20px; height:20px;" type="image/svg+xml"' +
+            'data="images/zillow-logo-mask.svg">, class="tooltip" title="' + name + '"></object>',
+            labelAnchor: new google.maps.Point(10, 10),
+            labelClass: 'marker'
+          }
+        }
+
+        var demoMarkers:Map.MarkerData[] = _.map(coordNames, function (name, index) {
+          return makeDemoMarker(triangleCoords[index], name);
+        });
+
+        $(document).trigger('markerData', [demoMarkers]);
 
         $(document).trigger('map:NewPolygon', [
           [{
             paths: triangleCoords,
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            //visible: false,
-            fillColor: '#FF0000',
+            strokeColor: '#0064CD',
+            strokeOpacity: 0.7,
+            strokeWeight: 1,
+            fillColor: '#0064CD',
             fillOpacity: 0.35
           }]
         ]);
